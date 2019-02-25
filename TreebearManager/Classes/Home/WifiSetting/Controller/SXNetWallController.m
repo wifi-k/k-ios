57c8d271ae3f41594bba3d41e3da8ac124fe8754
xuan <zhangran.xuan@treebear.cn>
2019-02-25 20:39:02 +0800

    1.更改页面

--- TreebearManager/Classes/Home/WifiSetting/Controller/SXNetWallController.m
+++ TreebearManager/Classes/Home/WifiSetting/Controller/SXNetWallController.m
@@ -7,6 +7,7 @@
 //
 
 #import "SXNetWallController.h"
+#import "SXWifiSettingCell.h"
 
 @interface SXNetWallController ()
 
@@ -21,6 +22,31 @@
     self.view.backgroundColor = SXColorRandom;
     
     self.navigationItem.title = @"防蹭网";
+}
+
+- (void)viewDidLayoutSubviews{
+    [super viewDidLayoutSubviews];
+    
+    self.tableView.frame = self.view.bounds;
+}
+
+#pragma mark -UITableViewDelegate/UITableViewDataSource-
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return 10;
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
+    return 80;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    SXWifiSettingCell *cell = [SXWifiSettingCell cellWithTableView:tableView];
+    return cell;
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath{
+    [tableView deselectRowAtIndexPath:indexPath animated:YES];
+    
 }
 
 @end
