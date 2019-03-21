49f4c30fb8b3d476690f523db7bf775a434a82c5
xuan <zhangran.xuan@treebear.cn>
2019-03-21 18:06:18 +0800

    1.生成时间

--- TreebearManager/Classes/Mine/PersonalInfo/Controller/SXPersonalInfoController.m
+++ TreebearManager/Classes/Mine/PersonalInfo/Controller/SXPersonalInfoController.m
@@ -126,10 +126,32 @@
     [self.navigationController pushViewController:forgetVC animated:YES];
 }
 
+- (NSString *)getNowTimeTimestamp{
+    
+    NSDateFormatter *formatter = [[NSDateFormatter alloc] init] ;
+    
+    [formatter setDateStyle:NSDateFormatterMediumStyle];
+    
+    [formatter setTimeStyle:NSDateFormatterShortStyle];
+    
+    [formatter setDateFormat:@"YYYY-MM-dd HH:mm:ss SSS"]; // ----------设置你想要的格式,hh与HH的区别:分别表示12小时制,24小时制
+    
+    //设置时区,这个对于时间的处理有时很重要
+    NSTimeZone* timeZone = [NSTimeZone timeZoneWithName:@"Asia/Shanghai"];
+    
+    [formatter setTimeZone:timeZone];
+    
+    NSDate *datenow = [NSDate date];//现在时间,你可以输出来看下是什么格式
+    
+    NSString *timeSp = [NSString stringWithFormat:@"%ld", (long)[datenow timeIntervalSince1970]*1000];
+    
+    return timeSp;
+}
+
 #pragma mark -上传文件-
 - (void)uploadImageData:(NSData *)data{
     WS(weakSelf);
-    NSString *key = @"user_avatar1";
+    NSString *key = [NSString stringWithFormat:@"xiaoki%@",[self getNowTimeTimestamp]];
     NSString *token = [SXPersonInfoModel sharedSXPersonInfoModel].qnToken;
     QNUploadManager *upManager = [[QNUploadManager alloc] init];
     //NSData *data = [@"Hello, World!" dataUsingEncoding:NSUTF8StringEncoding];
