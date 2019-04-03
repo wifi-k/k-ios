402e86f349c0290716b9acc35c2565f3da0ff38b
xuan <zhangran.xuan@treebear.cn>
2019-04-03 16:38:03 +0800

    1.图表绘制

--- TreebearManager/Classes/Home/Report/View/SXHomeReportReusableView.m
+++ TreebearManager/Classes/Home/Report/View/SXHomeReportReusableView.m
@@ -7,11 +7,9 @@
 //
 
 #import "SXHomeReportReusableView.h"
-#import "WYLineChartPoint.h"
-#import "WYChartCategory.h"
-#import "WYLineChartCalculator.h"
+#import "ORChartView.h"
 
-@interface SXHomeReportReusableView ()<WYLineChartViewDelegate,WYLineChartViewDatasource>
+@interface SXHomeReportReusableView ()
 @property (weak, nonatomic) IBOutlet UILabel *timeL;
 
 
@@ -19,7 +17,7 @@
 
 @property (weak, nonatomic) IBOutlet UILabel *appL;
 
-@property (nonatomic , weak) WYLineChartView *lineChart;//折线图
+@property (nonatomic , weak) ORChartView *lineChart;//折线图
 ///X轴
 @property (nonatomic, strong) NSArray *points;
 @property (nonatomic, strong) NSMutableArray *arrayValue;
@@ -29,21 +27,21 @@
 @implementation SXHomeReportReusableView
 
 #pragma mark -getter方法-
-- (NSMutableArray *)arrayValue{
-    if (_arrayValue == nil) {
-        NSArray *values = @[@1,@2,@3,@1,@2,@3,@2];
-        _arrayValue = [NSMutableArray arrayWithArray:values];
-    }
-    return _arrayValue;
-}
-
-- (NSMutableArray *)arrayTitle{
-    if (_arrayTitle == nil) {
-        NSArray *titles = @[@"10.1",@"10.2",@"10.3",@"10.4",@"10.5",@"10.6",@"10.7"];
-        _arrayTitle = [NSMutableArray arrayWithArray:titles];
-    }
-    return _arrayTitle;
-}
+//- (NSMutableArray *)arrayValue{
+//    if (_arrayValue == nil) {
+//        NSArray *values = @[@1,@2,@3,@1,@2,@3,@2];
+//        _arrayValue = [NSMutableArray arrayWithArray:values];
+//    }
+//    return _arrayValue;
+//}
+//
+//- (NSMutableArray *)arrayTitle{
+//    if (_arrayTitle == nil) {
+//        NSArray *titles = @[@"10.1",@"10.2",@"10.3",@"10.4",@"10.5",@"10.6",@"10.7"];
+//        _arrayTitle = [NSMutableArray arrayWithArray:titles];
+//    }
+//    return _arrayTitle;
+//}
 
 + (instancetype)sectionHeaderAwakeFromNib:(UICollectionView *)collectionView atIndexPath:(NSIndexPath *)indexPath {
     NSString *identifier = NSStringFromClass(self);
@@ -77,109 +75,40 @@
     self.chartBgView.layer.shadowRadius = 5;
     
     //2.添加图表
-    WYLineChartView *lineChart = [[WYLineChartView alloc] initWithFrame:CGRectMake(10, 10, SCREEN_WIDTH - 60, 160)];
-    lineChart.delegate = self;
-    lineChart.datasource = self;
-    lineChart.touchPointColor = SXColorBlue2;
-    lineChart.yAxisHeaderPrefix = nil;
-    lineChart.yAxisHeaderSuffix = nil;
-    lineChart.labelsFont = [UIFont systemFontOfSize:13];
-    lineChart.verticalReferenceLineColor = [UIColor colorWithWhite:0.7 alpha:1.0];
-    lineChart.horizontalRefernenceLineColor = [UIColor colorWithWhite:0.7 alpha:1.0];
-    lineChart.axisColor = [UIColor colorWithWhite:0.7 alpha:1.0];
-    lineChart.labelsColor = [UIColor colorWithWhite:0.7 alpha:1.0];
-    lineChart.animationDuration = 3.0f;
-    lineChart.animationStyle = kWYLineChartAnimationWidth;
-    lineChart.backgroundColor = UIColor.whiteColor;
-    lineChart.scrollable = YES;
-    lineChart.pinchable = YES;
+    ORChartView *lineChart = [[ORChartView alloc]initWithFrame:CGRectMake(10, 10, SCREEN_WIDTH - 60, 160) dataSource:@[@"123",@"88", @"45",@"33"] countFoyY:7];
+    lineChart.titleForX = @"日期/日";
+    lineChart.titleForY = @"收益/元";
+    [lineChart pointDidTapedCompletion:^(NSString *value, NSInteger index) {
+        NSLog(@"....%@....%ld", value, (long)index);
+    }];
     [self.chartBgView addSubview:lineChart];
     self.lineChart = lineChart;
     
-    //1.
+    //3.刷新表格
     [self updateGraph];
 }
 
 #pragma mark -重新绘制图表-
 - (void)updateGraph{
     
-    NSMutableArray *mutableArray = [NSMutableArray array];
-    NSArray *points = [WYLineChartPoint pointsFromValueArray:self.arrayValue];
-    [mutableArray addObject:points];
-    self.lineChart.points = [NSArray arrayWithArray:mutableArray];
-    self.points = mutableArray;
-    [self.lineChart updateGraph];
 }
 
-#pragma mark -WYLineChartViewDelegate代理-
-- (NSInteger)numberOfLabelOnXAxisInLineChartView:(WYLineChartView *)chartView {
-    return [self.points.firstObject count];
-}
-
-- (NSInteger)numberOfLabelOnYAxisInLineChartView:(WYLineChartView *)chartView {
-    return 1;
-}
-
-- (CGFloat)gapBetweenPointsHorizontalInLineChartView:(WYLineChartView *)chartView {
-    return 60.f;
-}
-
-- (NSInteger)numberOfReferenceLineVerticalInLineChartView:(WYLineChartView *)chartView {
-    return [self.points.firstObject count];
-}
-
-- (NSInteger)numberOfReferenceLineHorizontalInLineChartView:(WYLineChartView *)chartView {
-    return 1;
-}
-
-#pragma mark - WYLineChartViewDatasource代理 -
-- (NSString *)lineChartView:(WYLineChartView *)chartView contextTextForPointAtIndexPath:(NSIndexPath *)indexPath {
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
+    /*
+     随机数据源
+     随机样式
+     随机颜色
+     */
     
-    NSArray *pointsArray = self.lineChart.points[indexPath.section];
-    WYLineChartPoint *point = pointsArray[indexPath.row];
-    NSString *text = [NSString stringWithFormat:@"%lu", (NSInteger)point.value];
-    return text;
-}
-
-- (NSString *)lineChartView:(WYLineChartView *)chartView contentTextForXAxisLabelAtIndex:(NSInteger)index {
-    return self.arrayTitle[index];
-}
-
-- (WYLineChartPoint *)lineChartView:(WYLineChartView *)chartView pointReferToXAxisLabelAtIndex:(NSInteger)index {
-    return self.points.firstObject[index];
-}
-
-- (WYLineChartPoint *)lineChartView:(WYLineChartView *)chartView pointReferToVerticalReferenceLineAtIndex:(NSInteger)index {
-    return self.points.firstObject[index];
-}
-
-- (NSString *)lineChartView:(WYLineChartView *)chartView contentTextForYAxisLabelAtIndex:(NSInteger)index {
+    NSMutableArray *array = [NSMutableArray array];
+    for (int i = 0; i < 20; i ++) {
+        NSInteger num = arc4random() % 3000;
+        [array addObject:[NSString stringWithFormat:@"%ld",(long)num]];
+    }
     
-    CGFloat value = [self.lineChart.calculator minValuePointsOfLinesPointSet:self.lineChart.points].value;
-    return [NSString stringWithFormat:@"%lu", (NSInteger)value];
-}
-
-- (CGFloat)lineChartView:(WYLineChartView *)chartView valueReferToYAxisLabelAtIndex:(NSInteger)index {
-    
-    CGFloat value = [self.lineChart.calculator minValuePointsOfLinesPointSet:self.lineChart.points].value;
-    return value;
-}
-
-- (CGFloat)lineChartView:(WYLineChartView *)chartView valueReferToHorizontalReferenceLineAtIndex:(NSInteger)index {
-    
-    CGFloat value  = [self.lineChart.calculator minValuePointsOfLinesPointSet:self.lineChart.points].value;
-    return value;
-}
-
-- (NSDictionary *)lineChartView:(WYLineChartView *)chartView attributesForLineAtIndex:(NSUInteger)index {
-    
-    NSMutableDictionary *dict = [NSMutableDictionary dictionary];
-    dict[kWYLineChartLineAttributeLineStyle] = @1;
-    dict[kWYLineChartLineAttributeDrawGradient] = @1;
-    dict[kWYLineChartLineAttributeJunctionStyle] = @1;
-    dict[kWYLineChartLineAttributeLineColor] = SXColorRGBA(165.f, 203.f, 211.f, 0.9);;
-    dict[kWYLineChartLineAttributeJunctionColor] = SXColorRGBA(165.f, 203.f, 211.f, 0.9);
-    return dict;
+    self.lineChart.dataSource = [array copy];
+    self.lineChart.style = arc4random() % 4;
+    self.lineChart.lineColor = [UIColor colorWithRed:arc4random() % 255 /255.f green:arc4random() % 255 /255.f blue:arc4random() % 255 /255.f alpha:1];
 }
 
 @end
