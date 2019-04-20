622a10ee73a29daf5cc5824aca6a363ef3fd3a0c
xuan <zhangran.xuan@treebear.cn>
2019-04-20 14:59:16 +0800

    1.更换手机号

--- TreebearManager/Classes/Mine/PersonalInfo/Controller/SXMobileUpdateController.m
+++ TreebearManager/Classes/Mine/PersonalInfo/Controller/SXMobileUpdateController.m
@@ -57,7 +57,7 @@
         
         [MBProgressHUD showSuccessWithMessage:@"更换手机号成功!" toView:SXKeyWindow];
         
-        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(1.0f * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(2.0f * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
             [weakSelf.navigationController popViewControllerAnimated:YES];
         });
     } failure:^(NSError *error) {
